5172b1811831dfc646e66c40ebeda42c82249d49
danaskr <sakerdana1@gmail.com>
2024-10-30 16:20:32 +0100

    Add project files.

---
+++ Bag.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hdwj3h
+{
+    public class Item
+    {
+        public int frequency;
+        public string element;
+
+
+        public Item(string e,int f)
+        {
+            frequency = f;
+            element = e;
+        }
+        public Item() { }
+
+
+        public void readItem()
+        {
+
+            bool IntInput;
+
+            Console.WriteLine(" element: ");
+            element = Console.ReadLine();
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine(" frequency: ");
+                    IntInput = int.TryParse(Console.ReadLine(),out frequency); // must be true here
+                }
+                catch (System.FormatException)
+                {
+                    IntInput = false;
+                }
+
+            } while (!IntInput);
+
+        }
+
+       
+
+        public override string ToString()
+        {
+            return "(" + element + "," + frequency + ")"; 
+        }
+
+    }
+
+    public class Bag
+    {
+
+        List<Item> vec = new List<Item>();
+        
+        private Item mostFrequent;
+        
+
+        public class BagIsEmpty : Exception { }
+        public class ElementNotFound : Exception { }
+        public class ElementAlreadyExists : Exception { }
+
+        /// helpers
+        private bool isEmpty()
+        {
+            return vec.Count == 0;
+        }
+        public int getSize()
+        {
+            return vec.Count;
+        }
+
+        private Item findElem(string e)
+        {
+            if (isEmpty())
+            {
+                throw new BagIsEmpty();
+            }
+            Item I = new Item();
+            bool found = false;
+            for(int i =0; i< vec.Count; i++)
+            {
+                if (vec[i].element == e)
+                {
+                    I.element = e;
+                    I.frequency = vec[i].frequency;
+                    found = true;
+
+                }
+                
+            }
+
+            if (!found)
+            {
+                throw new ElementNotFound();
+            }
+            return I;
+
+        }
+
+        
+        private bool isElem(Item I)
+        {
+            if(isEmpty())
+                throw new BagIsEmpty();
+
+            bool found = false;
+            for(int i =0; i< vec.Count; i++)
+            {
+                if (vec[i].element == I.element) {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+        private bool isElem2(Item I)
+        {
+
+            bool found = false;
+            for (int i = 0; i < vec.Count; i++)
+            {
+                if (vec[i].element == I.element)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        /// 
+        public void insert(Item i)
+        {
+            if(isElem2(i)) throw new ElementAlreadyExists();
+
+            vec.Add(i);
+            mostFrequant();
+
+
+        }
+        public void remove(Item I)
+        {
+            if (isEmpty())
+                throw new BagIsEmpty();
+            if (!isElem(I))
+                throw new ElementNotFound();
+                
+         
+            List<Item> temp = new List<Item>();
+            for(int i =0; i< vec.Count; i++)
+            {
+                if (vec[i].element != I.element)
+                {
+                     
+                    temp.Add(vec[i]);
+                }
+                
+            }
+            
+            vec.Clear();
+            vec.AddRange(temp);
+
+            mostFrequant();
+        }
+        
+      
+        public int getFrequency(string e)
+        {
+            if (isEmpty())
+            {
+                throw new BagIsEmpty();
+            }
+            if(!isElem(findElem(e)))
+                throw new ElementNotFound();
+
+            return findElem(e).frequency;
+        }
+
+        public Item mostFrequant()
+        {
+            if (!isEmpty())
+            {
+                int ind = 0;
+                int maxFreq = 0;
+
+                for (int i = 1; i < vec.Count; i++)
+                {
+                    if (vec[i].frequency > maxFreq)
+                    {
+                        ind = i;
+                        maxFreq = vec[i].frequency;
+
+                    }
+                }
+                mostFrequent = vec[ind];
+                return mostFrequent;
+            }
+            else
+            {
+                throw new BagIsEmpty() ;
+            }
+        }
+
+        
+
+        public override string ToString()
+        {
+            string str = "[";
+            str = str + string.Join(",", vec);
+            str += "]\n";
+
+            return str;
+
+        }
+
+
+
+
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace hdwj3h
+{
+    internal class Menu
+    {
+        private Bag bag = new Bag();
+        public void Run()
+        {
+            int c;
+            do
+            {
+                c = GetMenu();
+                switch (c)
+                {
+                    case 1:
+                        Insert();
+                        PrintThatBag();
+                        break;
+                    case 2:
+                        Remove();
+                        PrintThatBag();
+                        break;
+                    case 3:
+                        GetFrequency();
+                        PrintThatBag();
+                        break;
+                    case 4:
+                        TheMostFrequent();
+                        break;
+                    case 5:
+                        PrintThatBag();
+                        break;
+                    default: 
+                        Console.WriteLine("viszontlatasra!");
+                        break;
+                }
+            } while (c != 0);
+        }
+        #region menu operations
+
+        /// 1 //////////////////////////////////
+
+        private void Insert()
+        {
+            Item i = new Item();
+            i.readItem();
+            try
+            {
+                bag.insert(i);
+                Console.WriteLine("inserted correctly :) ");
+            }
+            catch (Bag.ElementAlreadyExists)
+            {
+                Console.WriteLine("Element Already Exidts !");
+            }
+
+        }
+
+        /// 2 //////////////////////////////////
+
+        private void Remove()
+        {
+           
+            Item i = new Item();
+            i.readItem() ;
+            
+            try
+            {
+               
+                bag.remove(i) ;
+                Console.WriteLine("The element " + i + " has been removed");
+                
+            }
+            catch(Bag.BagIsEmpty){
+                Console.WriteLine("Bag is Empty !! ");
+            }
+            catch (Bag.ElementNotFound)
+            {
+                Console.WriteLine("Element not in the Bag! Couldn't remove </3 ");
+            }
+
+        }
+
+        /// 3 //////////////////////////////////
+
+        private void GetFrequency()
+        {
+            Console.WriteLine("element :");
+            string e =Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("The frequency is : " + bag.getFrequency(e));
+            }
+            catch (Bag.ElementNotFound)
+            {
+                Console.WriteLine("Element not in the Bag!, unable to get frequency :( ");
+            }
+            catch (Bag.BagIsEmpty)
+            {
+                Console.WriteLine("Bag is empty, unable to get frequency :( ");
+            }
+
+        }
+        ///  4 //////////////////////////////////
+
+        private void TheMostFrequent()
+        { Item i;
+            try{
+                i = bag.mostFrequant();
+                Console.WriteLine("the most frequent element is " + i);
+            }catch(Bag.BagIsEmpty)
+            {
+                Console.WriteLine("Bag is empty, mostFrequent couldn't be found :( ");
+            }
+        }
+
+        /// 5 //////////////////////////////////
+
+        private void PrintThatBag()
+        {
+            Console.WriteLine(bag);
+        }
+
+        /////////////////////////////////////
+
+
+        private static int GetMenu()
+        {
+            int c;
+            do
+            {
+                Console.WriteLine("\n****************Task 8 - Bag****************");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Insert an element");
+                Console.WriteLine("2. Remove an element");
+                Console.WriteLine("3. return the frequency of an element");
+                Console.WriteLine("4. get the most frequent element");
+                Console.WriteLine("5. Print out the Bag");
+                Console.WriteLine("**********************************************");
+
+                try
+                {
+                    c = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException) { c = -1; }
+            } while (c < 0 || c > 5); 
+            return c;
+        }
+        #endregion
+    }
+}
